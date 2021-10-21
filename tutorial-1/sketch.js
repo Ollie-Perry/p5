70f0a748acdc7ed4a668bd64e03ee799287da747
Ollie-Perry <oliver2.perry@live.uwe.ac.uk>
2021-10-21 12:35:36 +0100

    Update sketch.js

--- tutorial-1/sketch.js
+++ tutorial-1/sketch.js
@@ -1,31 +1,38 @@
-let rectWidth = 100;
-let rectHeight = 50;
+
 
 function setup(){
     createCanvas(displayWidth, displayHeight);
     background(50,60,70);
+    frameRate(20)
+    drawingContext.shadowOffsetX = 0;
+    drawingContext.shadowOffsetY = 0;
+    drawingContext.shadowBlur = 15;
+    drawingContext.shadowColor = 'black';
     //noLoop()
 }
 
 function draw(){
-    textSize(50);
-    text("Hello World", 100,100);
-    fill(random(0,255),random(0,255),random(0,255));
+    fill(random(10,100),random(10,100),random(10,100));
+    noStroke()
     rectMode(CENTER);
-    drawShape(rectWidth,rectHeight)
+    drawShape()
 }
 
-function drawShape(rectWidth,rectHeight){
+function drawShape(){
     let xPos = random(0, width);
 	let yPos = random(0, height);
-    fill(100, 30, 20);
-    
-    let choice = random(0,2);
-    if(choice > 1){
-        rect(xPos,yPos,rectWidth, rectHeight);
-    } else if (choice < 0.5) {
-        ellipse(xPos,yPos,rectWidth, rectHeight);
+    let xPos2 = random(0, width);
+	let yPos2 = random(0, height);
+    let xPos3 = random(0, width);
+	let yPos3 = random(0, height);
+    let shapeWidth = random(10,700);
+    let shapeHeight = random(10,700);
+    let choice = random(0,3);
+    if(choice < 1){
+        rect(xPos,yPos,shapeWidth, shapeHeight);
+    } else if (choice > 2) {
+        ellipse(xPos,yPos,shapeWidth, shapeHeight);
     } else {
-        triangle(xPos,yPos,rectWidth, rectHeight);
+        triangle(xPos,yPos,xPos2,yPos2,xPos3,yPos3);
     }
 }
